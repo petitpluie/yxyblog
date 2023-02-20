fb0fbd60b356c1727c526133aa54aaffc01b71c8
yangxiaoyu <petitepluie@126.com>
2023-02-20 12:52:26 +0800

    增加新建文章的功能

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, flash, url_for
 import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yxyblog_is_online'
 
 # 获取数据库连接
 def get_db_conn():
@@ -13,28 +14,43 @@ def get_db_conn():
 # 从数据库取文章数据
 def get_db_data_by_id(blog_id):
     conn = get_db_conn()
-    blog = conn.execute('select * from blog where id = ?', (blog_id,)).fetchone()
+    blog = conn.execute('select * from blog where id = ?',(blog_id,)).fetchone()
     return blog
 
 # 博客首页，查看博客列表
 @app.route('/')
 def index():
     conn = get_db_conn()
-    blogs = conn.execute('select * from blog').fetchall() # 从句柄获取data
+    blogs = conn.execute('select * from blog').fetchall()  # 从句柄获取data
     html = render_template('index.html', blogs = blogs)
     return html
 
 # 创建一篇博客
-@app.route('/blog/new', methods = ['GET', 'POST'])
+@app.route('/blog/new', methods=['GET', 'POST'])
 def new():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('请输入文章标题')
+        elif not content:
+            flash('请输入文章内容')
+        else:
+            conn = get_db_conn()
+            conn.execute(
+                'insert into blog (title,content) values (?,?)', (title, content))
+            conn.commit()
+            conn.close()
+            flash('文章保存成功')
+            return redirect(url_for('index'))
     return render_template('new.html')
 
 # 查看一篇博客
 @app.route('/blog/<int:blog_id>')
 def blog(blog_id):
     blog = get_db_data_by_id(blog_id)
-    return render_template('blog.html', blog = blog)
+    return render_template('blog.html', blog=blog)
 
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = 8090)
+    app.run(debug=False, host='0.0.0.0', port=8090)
