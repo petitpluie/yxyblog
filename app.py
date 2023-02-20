a09050186be6481c6ab80a0aa27712fe74bff251
yangxiaoyu <petitepluie@126.com>
2023-02-20 14:37:26 +0800

    YXY博客基本功能实现，增删改

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import Flask, redirect, render_template, request, flash, url_for
 import sqlite3
 
@@ -14,7 +15,7 @@ def get_db_conn():
 # 从数据库取文章数据
 def get_db_data_by_id(blog_id):
     conn = get_db_conn()
-    blog = conn.execute('select * from blog where id = ?',(blog_id,)).fetchone()
+    blog = conn.execute('select * from blog where id = ?',(blog_id, )).fetchone()
     return blog
 
 # 博客首页，查看博客列表
@@ -51,6 +52,44 @@ def blog(blog_id):
     blog = get_db_data_by_id(blog_id)
     return render_template('blog.html', blog=blog)
 
+# 删除一篇博客
+@app.route('/blog/<int:blog_id>/delete', methods=['POST',])
+def delete(blog_id):
+    blog = get_db_data_by_id(blog_id)
+    if(not blog):
+        flash('文章不存在')
+    conn = get_db_conn()
+    conn.execute('DELETE FROM blog WHERE id = ?',(blog_id,))
+    conn.commit()
+    conn.close()
+    flash('文章删除成功')
+    return redirect(url_for('index'))
+
+# 修改一篇文章
+@app.route('/blog/<int:blog_id>/edit', methods=['GET','POST'])
+def edit(blog_id):
+    blog = get_db_data_by_id(blog_id)
+    if(not blog):
+        flash('文章不存在')
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('标题不能为空')
+        elif not content:
+            flash('内容不能为空')
+        else:
+            conn = get_db_conn()
+            conn.execute('UPDATE blog SET title = ?, content = ? WHERE id = ?', (title, content, blog_id))
+            conn.commit()
+            conn.close()
+            flash('修改已保存')
+            return redirect(url_for('index'))
+    return render_template('edit.html', blog = blog)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8090)
+    app.run(debug=False, host='0.0.0.0', port=8090)
